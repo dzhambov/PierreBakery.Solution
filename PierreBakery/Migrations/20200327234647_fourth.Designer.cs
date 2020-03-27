af8ce095bef17121bd2f25455dba5deedf73fd22
Hristo Dzhambov <dzhambov@hotmail.com>
2020-03-27 19:58:02 -0400

    add Index page for treats.

--- PierreBakery/Migrations/20200327234647_fourth.Designer.cs
+++ PierreBakery/Migrations/20200327234647_fourth.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PierreBakery.Models;
 
 namespace PierreBakery.Migrations
 {
     [DbContext(typeof(PierreBakeryContext))]
-    partial class PierreBakeryContextModelSnapshot : ModelSnapshot
+    [Migration("20200327234647_fourth")]
+    partial class fourth
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
